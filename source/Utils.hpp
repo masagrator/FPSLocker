7f4bc8f5752e9784925ad31eb20321a226842e29
MasaGratoR <masagrator@gmail.com>
2023-03-18 17:28:40 +0100

    Support yaml conversion (#3)

---
+++ source/Utils.hpp
@@ -0,0 +1,180 @@
+#pragma once
+
+uint8_t* FPS_shared = 0;
+uint8_t* FPSmode_shared = 0;
+uint8_t* FPSlocked_shared = 0;
+bool* pluginActive = 0;
+bool* ZeroSync_shared = 0;
+bool _isDocked = false;
+bool _def = true;
+bool PluginRunning = false;
+bool state = false;
+bool closed = false;
+bool check = false;
+bool SaltySD = false;
+bool bak = false;
+bool plugin = false;
+char FPSMode_c[64];
+char FPSTarget_c[32];
+char PFPS_c[32];
+char SyncWait_c[32];
+
+char configPath[128] = "";
+char patchPath[128] = "";
+char savePath[64] = "";
+uint64_t PID = 0;
+uint64_t TID = 0;
+uint64_t BID = 0;
+
+Handle remoteSharedMemory = 1;
+SharedMemory _sharedmemory = {};
+bool SharedMemoryUsed = false;
+
+Result configValid = 10;
+Result patchValid = 0x202;
+char lockInvalid[32] = "";
+char patchChar[64] = "";
+char patchAppliedChar[64] = "";
+bool* patchApplied_shared = 0;
+Thread t0;
+bool threadActive = true;
+
+struct Title
+{
+	uint64_t TitleID;
+	std::string TitleName;
+};
+
+std::vector<Title> titles;
+
+uint64_t checkFile(const char* path) {
+    FILE* file = fopen(path, "rb");
+    if (!file)
+        return 0x202;
+    fclose(file);
+    return 0;
+}
+
+uint64_t getBID() {
+	u64 BID_temp = 0;
+
+	if (R_SUCCEEDED(ldrDmntInitialize())) {
+		LoaderModuleInfo* module_infos = (LoaderModuleInfo*)malloc(sizeof(LoaderModuleInfo) * 16);
+		s32 module_infos_count = 0;
+		Result ret = ldrDmntGetProcessModuleInfo(PID, module_infos, 16, &module_infos_count);
+		ldrDmntExit();
+
+		if (R_SUCCEEDED(ret)) for (int itr = 0; itr < module_infos_count; itr++) {
+			static u64 comp_address = 0;
+			ret = 0xFFDE;
+			if (!comp_address) {
+				comp_address = module_infos[itr].base_address;
+				continue;
+			}
+			if (module_infos[itr].base_address - comp_address == 0x4000) {
+				for (int itr2 = 0; itr2 < 8; itr2++) {
+					*(uint8_t*)((uint64_t)&BID_temp+itr2) = module_infos[itr].build_id[itr2];
+				}
+				BID_temp = __builtin_bswap64(BID_temp);
+				itr = module_infos_count;
+			}
+			else comp_address = module_infos[itr].base_address;
+		}
+		free(module_infos);
+	}
+
+	return BID_temp;
+}
+
+bool LoadSharedMemory() {
+	if (SaltySD_Connect())
+		return false;
+
+	SaltySD_GetSharedMemoryHandle(&remoteSharedMemory);
+	SaltySD_Term();
+
+	shmemLoadRemote(&_sharedmemory, remoteSharedMemory, 0x1000, Perm_Rw);
+	if (!shmemMap(&_sharedmemory)) {
+		SharedMemoryUsed = true;
+		return true;
+	}
+	return false;
+}
+
+ptrdiff_t searchSharedMemoryBlock(uintptr_t base) {
+	ptrdiff_t search_offset = 0;
+	while(search_offset < 0x1000) {
+		uint32_t* MAGIC_shared = (uint32_t*)(base + search_offset);
+		if (*MAGIC_shared == 0x465053) {
+			return search_offset;
+		}
+		else search_offset += 4;
+	}
+	return -1;
+}
+
+bool CheckPort () {
+	Handle saltysd;
+	for (int i = 0; i < 67; i++) {
+		if (R_SUCCEEDED(svcConnectToNamedPort(&saltysd, "InjectServ"))) {
+			svcCloseHandle(saltysd);
+			break;
+		}
+		else {
+			if (i == 66) return false;
+			svcSleepThread(1'000'000);
+		}
+	}
+	for (int i = 0; i < 67; i++) {
+		if (R_SUCCEEDED(svcConnectToNamedPort(&saltysd, "InjectServ"))) {
+			svcCloseHandle(saltysd);
+			return true;
+		}
+		else svcSleepThread(1'000'000);
+	}
+	return false;
+}
+
+std::string getAppName(uint64_t Tid)
+{
+	NsApplicationControlData appControlData;
+	size_t appControlDataSize = 0;
+	if (R_FAILED(nsGetApplicationControlData(NsApplicationControlSource::NsApplicationControlSource_Storage, Tid, &appControlData, sizeof(NsApplicationControlData), &appControlDataSize))) {
+		char returnTID[18];
+		sprintf(returnTID, "%016lx-", Tid);
+		return (std::string)returnTID;
+	}
+	
+	NacpLanguageEntry *languageEntry = nullptr;
+	Result rc = nsGetApplicationDesiredLanguage(&appControlData.nacp, &languageEntry);
+	if (R_FAILED(rc)) {
+		char returnTID[18];
+		sprintf(returnTID, "0x%X", rc);
+		return (std::string)returnTID;
+	}
+	
+	return std::string(languageEntry->name);
+}
+
+Result getTitles(int32_t count)
+{
+  NsApplicationRecord appRecords = {};
+  int32_t actualAppRecordCnt = 0;
+  Result rc;
+
+  while (1)
+  {
+    static int32_t offset = 0;
+    rc = nsListApplicationRecord(&appRecords, 1, offset, &actualAppRecordCnt);
+    if (R_FAILED(rc) || (actualAppRecordCnt < 1) || (offset >= count)) break;
+    if (appRecords.application_id != 0) {
+        Title title;
+        title.TitleID = appRecords.application_id;
+        title.TitleName = getAppName(appRecords.application_id);
+        titles.push_back(title);
+    }
+    offset++;
+    appRecords = {};
+  }
+  return rc;
+}
--- source/main.cpp
+++ source/main.cpp
@@ -1,80 +1,312 @@
 #define TESLA_INIT_IMPL // If you have more than one file using the tesla header, only define this in the main one
 #include <tesla.hpp>    // The Tesla Header
 #include <sys/stat.h>
+#include <dirent.h>
 #include "SaltyNX.h"
+#include "Lock.hpp"
+#include "Utils.hpp"
 
-uint8_t* FPS_shared = 0;
-uint8_t* FPSmode_shared = 0;
-uint8_t* FPSlocked_shared = 0;
-bool* pluginActive = 0;
-bool* ZeroSync_shared = 0;
-bool _isDocked = false;
-bool _def = true;
-bool PluginRunning = false;
-bool state = false;
-bool closed = false;
-bool check = false;
-bool SaltySD = false;
-bool bak = false;
-bool plugin = false;
-char FPSMode_c[64];
-char FPSTarget_c[32];
-char PFPS_c[32];
-char SyncWait_c[32];
-uint64_t PID = 0;
-uint64_t TID = 0;
-Handle remoteSharedMemory = 1;
-SharedMemory _sharedmemory = {};
-bool SharedMemoryUsed = false;
-
-bool LoadSharedMemory() {
-	if (SaltySD_Connect())
-		return false;
-
-	SaltySD_GetSharedMemoryHandle(&remoteSharedMemory);
-	SaltySD_Term();
-
-	shmemLoadRemote(&_sharedmemory, remoteSharedMemory, 0x1000, Perm_Rw);
-	if (!shmemMap(&_sharedmemory)) {
-		SharedMemoryUsed = true;
-		return true;
+void loopThread(void*) {
+	while(threadActive) {
+		if (R_FAILED(pmdmntGetApplicationProcessId(&PID))) break;
+		svcSleepThread(125'000'000);
 	}
-	return false;
+	PluginRunning = false;
+	check = false;
+	closed = true;
 }
 
-ptrdiff_t searchSharedMemoryBlock(uintptr_t base) {
-	ptrdiff_t search_offset = 0;
-	while(search_offset < 0x1000) {
-		uint32_t* MAGIC_shared = (uint32_t*)(base + search_offset);
-		if (*MAGIC_shared == 0x465053) {
-			return search_offset;
+class AdvancedGui : public tsl::Gui {
+public:
+    AdvancedGui() {
+		configValid = LOCK::readConfig(&configPath[0]);
+		if (R_FAILED(configValid))
+			sprintf(&lockInvalid[0], "Config error: 0x%X", configValid);
+		else {
+			patchValid = checkFile(&patchPath[0]);
+			if (R_FAILED(patchValid))
+				sprintf(&patchChar[0], "Patch file doesn't exist.");
+			else sprintf(&patchChar[0], "Patch file exists.");
 		}
-		else search_offset += 4;
 	}
-	return -1;
-}
 
-bool CheckPort () {
-	Handle saltysd;
-	for (int i = 0; i < 67; i++) {
-		if (R_SUCCEEDED(svcConnectToNamedPort(&saltysd, "InjectServ"))) {
-			svcCloseHandle(saltysd);
-			break;
+    virtual tsl::elm::Element* createUI() override {
+        auto frame = new tsl::elm::OverlayFrame("FPSLocker", "Advanced settings");
+
+		auto list = new tsl::elm::List();
+
+		list->addItem(new tsl::elm::CustomDrawer([](tsl::gfx::Renderer *renderer, s32 x, s32 y, s32 w, s32 h) {
+
+			if (R_SUCCEEDED(configValid)) {
+				renderer->drawString("Found valid config file!", false, x, y+20, 20, renderer->a(0xFFFF));
+				renderer->drawString(&patchChar[0], false, x, y+40, 20, renderer->a(0xFFFF));
+				renderer->drawString(&patchAppliedChar[0], false, x, y+60, 20, renderer->a(0xFFFF));
+			}
+			else
+				renderer->drawString(&lockInvalid[0], false, x, y+20, 20, renderer->a(0xFFFF));
+				
+
+		}), 70);
+
+		if (*FPSmode_shared != 255) {
+			list->addItem(new tsl::elm::CategoryHeader("NVN", true));
+			auto *clickableListItem3 = new tsl::elm::ToggleListItem("Sync Wait", !*ZeroSync_shared);
+			clickableListItem3->setClickListener([](u64 keys) { 
+				if ((keys & HidNpadButton_A) && PluginRunning) {
+					*ZeroSync_shared = !*ZeroSync_shared;
+					return true;
+				}
+				return false;
+			});
+			list->addItem(clickableListItem3);
+		}
+
+		if (R_SUCCEEDED(configValid)) {
+			list->addItem(new tsl::elm::CategoryHeader("Patch will be applied on next game boot", true));
+			auto *clickableListItem = new tsl::elm::ListItem("Convert config to patch file");
+			clickableListItem->setClickListener([](u64 keys) { 
+				if ((keys & HidNpadButton_A) && PluginRunning) {
+					patchValid = LOCK::createPatch(&patchPath[0]);
+					if (R_SUCCEEDED(patchValid)) {
+						sprintf(&patchChar[0], "Patch file created successfully.");
+					}
+					else sprintf(&patchChar[0], "Error while creating patch: 0x%x", patchValid);
+					return true;
+				}
+				return false;
+			});
+			list->addItem(clickableListItem);
+
+			list->addItem(new tsl::elm::CategoryHeader("Patch won't be applied on next game boot", true));
+			auto *clickableListItem2 = new tsl::elm::ListItem("Delete patch file");
+			clickableListItem2->setClickListener([](u64 keys) { 
+				if ((keys & HidNpadButton_A) && PluginRunning) {
+					if (R_SUCCEEDED(patchValid)) {
+						remove(&patchPath[0]);
+						patchValid = 0x202;
+						sprintf(&patchChar[0], "Patch file deleted successfully.");
+					}
+					return true;
+				}
+				return false;
+			});
+			list->addItem(clickableListItem2);
+		}
+
+		frame->setContent(list);
+
+        return frame;
+    }
+
+	virtual void update() override {
+		static uint8_t i = 10;
+
+		if (PluginRunning) {
+			if (i > 9) {
+				if (*patchApplied_shared) {
+					sprintf(patchAppliedChar, "Plugin loaded patch to game");
+				}
+				else sprintf(patchAppliedChar, "Plugin didn't apply patch to game");
+				i = 0;
+			}
+			else i++;
+		}
+	}
+};
+
+class NoGameSub : public tsl::Gui {
+public:
+	uint64_t _titleid = 0;
+	char _titleidc[17] = "";
+	std::string _titleName = "";
+
+	NoGameSub(uint64_t titleID, std::string titleName) {
+		_titleid = titleID;
+		sprintf(&_titleidc[0], "%016lX", _titleid);
+		_titleName = titleName;
+	}
+
+	// Called when this Gui gets loaded to create the UI
+	// Allocate all elements on the heap. libtesla will make sure to clean them up when not needed anymore
+	virtual tsl::elm::Element* createUI() override {
+		// A OverlayFrame is the base element every overlay consists of. This will draw the default Title and Subtitle.
+		// If you need more information in the header or want to change it's look, use a HeaderOverlayFrame.
+		auto frame = new tsl::elm::OverlayFrame(_titleidc, _titleName);
+
+		// A list that can contain sub elements and handles scrolling
+		auto list = new tsl::elm::List();
+
+		auto *clickableListItem = new tsl::elm::ListItem("Delete settings");
+		clickableListItem->setClickListener([this](u64 keys) { 
+			if (keys & HidNpadButton_A) {
+				char path[512] = "";
+				if (_titleid != 0x1234567890ABCDEF) {
+					sprintf(&path[0], "sdmc:/SaltySD/plugins/FPSLocker/%016lx.dat", _titleid);
+					remove(path);
+				}
+				else {
+					struct dirent *entry;
+    				DIR *dp;
+					sprintf(&path[0], "sdmc:/SaltySD/plugins/FPSLocker/");
+
+					dp = opendir(path);
+					if (!dp)
+						return true;
+					while ((entry = readdir(dp))) {
+						if (entry -> d_type != DT_DIR && std::string(entry -> d_name).find(".dat") != std::string::npos) {
+							sprintf(&path[0], "sdmc:/SaltySD/plugins/FPSLocker/%s", entry->d_name);
+							remove(path);
+						}
+					}
+					closedir(dp);
+				}
+				return true;
+			}
+			return false;
+		});
+
+		list->addItem(clickableListItem);
+
+		auto *clickableListItem2 = new tsl::elm::ListItem("Delete patches");
+		clickableListItem2->setClickListener([this](u64 keys) { 
+			if (keys & HidNpadButton_A) {
+				char folder[640] = "";
+				if (_titleid != 0x1234567890ABCDEF) {
+					sprintf(&folder[0], "sdmc:/SaltySD/plugins/FPSLocker/patches/%016lx/", _titleid);
+
+					struct dirent *entry;
+    				DIR *dp;
+
+					dp = opendir(folder);
+					if (!dp)
+						return true;
+					while ((entry = readdir(dp))) {
+						if (entry -> d_type != DT_DIR && std::string(entry -> d_name).find(".bin") != std::string::npos) {
+							sprintf(&folder[0], "sdmc:/SaltySD/plugins/FPSLocker/patches/%016lx/%s", _titleid, entry -> d_name);
+							remove(folder);
+						}
+					}
+					closedir(dp);
+				}
+				else {
+					struct dirent *entry;
+					struct dirent *entry2;
+    				DIR *dp;
+					DIR *dp2;
+
+					sprintf(&folder[0], "sdmc:/SaltySD/plugins/FPSLocker/patches/");
+					dp = opendir(folder);
+					if (!dp)
+						return true;
+					while ((entry = readdir(dp))) {
+						if (entry -> d_type != DT_DIR)
+							continue;
+						sprintf(&folder[0], "sdmc:/SaltySD/plugins/FPSLocker/patches/%s/", entry -> d_name);
+						dp2 = opendir(folder);
+						while ((entry2 = readdir(dp2))) {
+							if (entry2 -> d_type != DT_DIR && std::string(entry2 -> d_name).find(".bin") != std::string::npos) {
+								sprintf(&folder[0], "sdmc:/SaltySD/plugins/FPSLocker/patches/%s/%s", entry -> d_name, entry2 -> d_name);
+								remove(folder);
+							}
+						}
+						closedir(dp2);
+					}
+					closedir(dp);
+				}
+				return true;
+			}
+			return false;
+		});
+
+		list->addItem(clickableListItem2);
+
+		frame->setContent(list);
+
+		return frame;
+	}
+};
+
+class NoGame : public tsl::Gui {
+public:
+
+	Result rc = 1;
+	NoGame(Result result, u8 arg2, bool arg3) {
+		rc = result;
+	}
+
+	// Called when this Gui gets loaded to create the UI
+	// Allocate all elements on the heap. libtesla will make sure to clean them up when not needed anymore
+	virtual tsl::elm::Element* createUI() override {
+		// A OverlayFrame is the base element every overlay consists of. This will draw the default Title and Subtitle.
+		// If you need more information in the header or want to change it's look, use a HeaderOverlayFrame.
+		auto frame = new tsl::elm::OverlayFrame("FPSLocker", APP_VERSION);
+
+		// A list that can contain sub elements and handles scrolling
+		auto list = new tsl::elm::List();
+
+		list->addItem(new tsl::elm::CustomDrawer([](tsl::gfx::Renderer *renderer, s32 x, s32 y, s32 w, s32 h) {
+			if (!SaltySD) {
+				renderer->drawString("SaltyNX is not working!", false, x, y+20, 20, renderer->a(0xF33F));
+			}
+			else if (!plugin) {
+				renderer->drawString("Can't detect NX-FPS plugin on sdcard!", false, x, y+20, 20, renderer->a(0xF33F));
+			}
+			else if (!check) {
+				renderer->drawString("Game is not running!", false, x, y+20, 19, renderer->a(0xF33F));
+			}
+		}), 30);
+
+		if (R_FAILED(rc)) {
+			char error[24] = "";
+			sprintf(&error[0], "Err: 0x%x", rc);
+			auto *clickableListItem2 = new tsl::elm::ListItem(error);
+			clickableListItem2->setClickListener([](u64 keys) { 
+				if (keys & HidNpadButton_A) {
+					return true;
+				}
+				return false;
+			});
+
+			list->addItem(clickableListItem2);
 		}
 		else {
-			if (i == 66) return false;
-			svcSleepThread(1'000'000);
+			auto *clickableListItem3 = new tsl::elm::ListItem("All");
+			clickableListItem3->setClickListener([](u64 keys) { 
+				if (keys & HidNpadButton_A) {
+					tsl::changeTo<NoGameSub>(0x1234567890ABCDEF, "Everything");
+					return true;
+				}
+				return false;
+			});
+
+			list->addItem(clickableListItem3);
+
+			for (size_t i = 0; i < titles.size(); i++) {
+				auto *clickableListItem = new tsl::elm::ListItem(titles[i].TitleName);
+				clickableListItem->setClickListener([i](u64 keys) { 
+					if (keys & HidNpadButton_A) {
+						tsl::changeTo<NoGameSub>(titles[i].TitleID, titles[i].TitleName);
+						return true;
+					}
+					return false;
+				});
+
+				list->addItem(clickableListItem);
+			}
 		}
+
+		frame->setContent(list);
+
+		return frame;
 	}
-	for (int i = 0; i < 67; i++) {
-		if (R_SUCCEEDED(svcConnectToNamedPort(&saltysd, "InjectServ"))) {
-			svcCloseHandle(saltysd);
-			return true;
-		}
-		else svcSleepThread(1'000'000);
+
+	virtual void update() override {}
+
+	// Called once every frame to handle inputs not handled by other UI elements
+	virtual bool handleInput(u64 keysDown, u64 keysHeld, const HidTouchState &touchPos, HidAnalogStickState joyStickPosLeft, HidAnalogStickState joyStickPosRight) override {
+		return false;   // Return true here to singal the inputs have been consumed
 	}
-	return false;
-}
+};
 
 class GuiTest : public tsl::Gui {
 public:
@@ -109,13 +341,18 @@ public:
 				renderer->drawString("Game is running.", false, x, y+20, 20, renderer->a(0xFFFF));
 				renderer->drawString("NX-FPS is not running!", false, x, y+40, 20, renderer->a(0xF33F));
 			}
+			else if (!*pluginActive) {
+				renderer->drawString("NX-FPS is running, but no frame was processed.", false, x, y+20, 20, renderer->a(0xF33F));
+				renderer->drawString("Restart overlay to check again.", false, x, y+50, 20, renderer->a(0xFFFF));
+			}
 			else {
-				renderer->drawString("NX-FPS plugin is running.", false, x, y+20, 20, renderer->a(0xFFFF));
-				renderer->drawString(FPSMode_c, false, x, y+40, 20, renderer->a(0xFFFF));
+				renderer->drawString("NX-FPS is running.", false, x, y+20, 20, renderer->a(0xFFFF));
+				if (*FPSmode_shared != 255)
+					renderer->drawString(FPSMode_c, false, x, y+40, 20, renderer->a(0xFFFF));
 				renderer->drawString(FPSTarget_c, false, x, y+60, 20, renderer->a(0xFFFF));
 				renderer->drawString(PFPS_c, false, x+290, y+48, 50, renderer->a(0xFFFF));
 			}
-	}), 100);
+		}), 90);
 
 		if (PluginRunning && *pluginActive) {
 			auto *clickableListItem = new tsl::elm::ListItem("Increase FPS target");
@@ -167,10 +404,10 @@ public:
 			});
 			list->addItem(clickableListItem4);
 
-			auto *clickableListItem3 = new tsl::elm::ToggleListItem("Sync Wait (!)", !*ZeroSync_shared);
+			auto *clickableListItem3 = new tsl::elm::ListItem("Advanced settings");
 			clickableListItem3->setClickListener([](u64 keys) { 
 				if ((keys & HidNpadButton_A) && PluginRunning) {
-					*ZeroSync_shared = !*ZeroSync_shared;
+					tsl::changeTo<AdvancedGui>();
 					return true;
 				}
 				return false;
@@ -180,15 +417,12 @@ public:
 			auto *clickableListItem5 = new tsl::elm::ListItem("Save settings");
 			clickableListItem5->setClickListener([](u64 keys) { 
 				if ((keys & HidNpadButton_A) && PluginRunning) {
-					char path[64];
-					pminfoGetProgramId(&TID, PID);
-					sprintf(&path[0], "sdmc:/SaltySD/plugins/FPSLocker/%016lX.dat", TID);
 					if (!*FPSlocked_shared && !*ZeroSync_shared) {
-						remove(path);
+						remove(savePath);
 					}
 					else {
 						mkdir("sdmc:/SaltySD/plugins/FPSLocker/", 777);
-						FILE* file = fopen(path, "wb");
+						FILE* file = fopen(savePath, "wb");
 						if (file) {
 							fwrite(FPSlocked_shared, 1, 1, file);
 							fwrite(ZeroSync_shared, 1, 1, file);
@@ -204,7 +438,7 @@ public:
 
 		// Add the list to the frame for it to be drawn
 		frame->setContent(list);
-        
+		
 		// Return the frame to have it become the top level element of this Gui
 		return frame;
 	}
@@ -212,12 +446,6 @@ public:
 	// Called once every frame to update values
 	virtual void update() override {
 		static uint8_t i = 10;
-		Result rc = pmdmntGetApplicationProcessId(&PID);
-		if (R_FAILED(rc) && PluginRunning) {
-			PluginRunning = false;
-			check = false;
-			closed = true;
-		}
 
 		if (PluginRunning) {
 			if (i > 9) {
@@ -244,7 +472,6 @@ public:
 			}
 			else i++;
 		}
-	
 	}
 
 	// Called once every frame to handle inputs not handled by other UI elements
@@ -281,12 +508,22 @@ public:
 				ptrdiff_t rel_offset = searchSharedMemoryBlock(base);
 				if (rel_offset > -1) {
 					pminfoInitialize();
+					pminfoGetProgramId(&TID, PID);
+					pminfoExit();
+					BID = getBID();
+					sprintf(&patchPath[0], "sdmc:/SaltySD/plugins/FPSLocker/patches/%016lX/%016lX.bin", TID, BID);
+					sprintf(&configPath[0], "sdmc:/SaltySD/plugins/FPSLocker/patches/%016lX/%016lX.yaml", TID, BID);
+					sprintf(&savePath[0], "sdmc:/SaltySD/plugins/FPSLocker/%016lX.dat", TID);
+
 					FPS_shared = (uint8_t*)(base + rel_offset + 4);
 					pluginActive = (bool*)(base + rel_offset + 9);
 					FPSlocked_shared = (uint8_t*)(base + rel_offset + 10);
 					FPSmode_shared = (uint8_t*)(base + rel_offset + 11);
 					ZeroSync_shared = (bool*)(base + rel_offset + 12);
+					patchApplied_shared = (bool*)(base + rel_offset + 13);
 					PluginRunning = true;
+					threadCreate(&t0, loopThread, NULL, NULL, 0x100, 0x20, 0);
+					threadStart(&t0);
 				}		
 			}
 		
@@ -295,8 +532,11 @@ public:
 	}  // Called at the start to initialize all services necessary for this Overlay
 	
 	virtual void exitServices() override {
-		pminfoExit();
+		threadActive = false;
+		threadWaitForExit(&t0);
+		threadClose(&t0);
 		shmemClose(&_sharedmemory);
+		nsExit();
 		fsdevUnmountDevice("sdmc");
 	}  // Callet at the end to clean up all services previously initialized
 
@@ -305,10 +545,19 @@ public:
 	virtual void onHide() override {}    // Called before overlay wants to change from visible to invisible state
 
 	virtual std::unique_ptr<tsl::Gui> loadInitialGui() override {
-		return initially<GuiTest>(1, 2, true);  // Initial Gui to load. It's possible to pass arguments to it's constructor like this
+		if (SaltySD && check && plugin) {
+			return initially<GuiTest>(1, 2, true);  // Initial Gui to load. It's possible to pass arguments to it's constructor like this
+		}
+		else {
+			tsl::hlp::doWithSmSession([]{
+				nsInitialize();
+			});
+			Result rc = getTitles(32);
+			return initially<NoGame>(rc, 2, true);
+		}
 	}
 };
 
 int main(int argc, char **argv) {
-    return tsl::loop<OverlayTest>(argc, argv);
+	return tsl::loop<OverlayTest>(argc, argv);
 }
