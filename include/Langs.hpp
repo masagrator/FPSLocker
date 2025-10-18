6d6c5bb11587561c80179e3869d012a0903463b9
MasaGratoR <masagrator@gmail.com>
2025-10-18 21:36:50 +0200

    Add files via upload

--- include/Langs.hpp
+++ include/Langs.hpp
@@ -1,4 +1,20 @@
 extern SetLanguage language;
 
-const char* getStringID(std::size_t id);
+namespace Lang {
+    enum Id {
+        Id_DeleteSettings,
+        Id_DeletePatches,
+        Id_SaltyNXIsNotWorking,
+        Id_CantDetectNXFPSPluginOnSdcard,
+        Id_GameIsNotRunning,
+        Id_All,
+        Id_Everything,
+        Id_GamesList,
+        Id_DisplaySettings,
+        Id_ChangeFPSTarget,
+        Id_GameWasClosedOverlayDisabled,
+    };
+}
+
+const char* getStringID(size_t id);
 const char* getTeslaStringID(std::size_t id);
