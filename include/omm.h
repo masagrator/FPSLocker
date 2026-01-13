3aee89262310f9906ecb2430c8aa2b52243ddd83
MasaGratoR <masagrator@gmail.com>
2026-01-13 06:44:44 +0100

    Add files via upload

--- include/omm.h
+++ include/omm.h
@@ -10,6 +10,7 @@ Result ommInitialize(void);
 void ommExit(void);
 Service* ommGetServiceSession(void);
 Result ommGetDefaultDisplayResolution(s32* width, s32* height);
+Result ommGetOperationMode(AppletOperationMode* s);
 
 #ifdef __cplusplus
 } // extern "C"
