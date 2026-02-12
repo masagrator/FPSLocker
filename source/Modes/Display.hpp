b8a1080b7adcc8dbe94d7a7c196f4823cfeb1393
MasaGratoR <masagrator@gmail.com>
2026-02-12 14:17:16 +0100

    Add files via upload

--- source/Modes/Display.hpp
+++ source/Modes/Display.hpp
@@ -648,16 +648,20 @@ public:
 
 		list->addItem(new tsl::elm::CustomDrawer([this](tsl::gfx::Renderer *renderer, s32 x, s32 y, s32 w, s32 h) {
 
-			auto dimensions = renderer->drawString(Docked_c, false, x, y+20, 20, renderer->a(0xFFFF));
-			auto string_width = dimensions.first;
+			const u32 fontsize = 20;
+			renderer->drawString(Docked_c, false, x, y+fontsize, fontsize, renderer->a(0xFFFF));
+			const char* start = strchr(Docked_c, '\n') + 1;
+			const char* end = strchr(start, '\n');
+			std::string second_line(start, end - start);
+			auto string_width = renderer->drawString(second_line.c_str(), false, x, y+fontsize, fontsize, renderer->a(0x0000)).first;
 			if (!block) {
 				if (laneCount == 2 || laneCount == 4) {
-					if ((linkRate * laneCount) < 40) renderer->drawString("\uE14C", false, x+20+string_width, y+40, 20, renderer->a(0xF00F));
-					else renderer->drawString("\uE14B", false, x+20+string_width, y+40, 20, renderer->a(0xF0F0));
+					if ((linkRate * laneCount) < 40) renderer->drawString("\uE14C", false, x+fontsize+string_width, y+(fontsize*2), fontsize, renderer->a(0xF00F));
+					else renderer->drawString("\uE14B", false, x+fontsize+string_width, y+(fontsize*2), fontsize, renderer->a(0xF0F0));
 				}
 				else {
-					if (linkRate < 20) renderer->drawString("\uE14C", false, x+20+string_width, y+40, 20, renderer->a(0xF00F));
-					else renderer->drawString("\uE14B", false, x+20+string_width, y+40, 20, renderer->a(0xF0F0));
+					if (linkRate < 20) renderer->drawString("\uE14C", false, x+fontsize+string_width, y+(fontsize*2), fontsize, renderer->a(0xF00F));
+					else renderer->drawString("\uE14B", false, x+fontsize+string_width, y+(fontsize*2), fontsize, renderer->a(0xF0F0));
 				}
 			}
 
